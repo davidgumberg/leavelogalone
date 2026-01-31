858c53738c97db4ee43c368b9ca7732e50b3b5eb
David Gumberg <davidzgumberg@gmail.com>
2026-01-30 18:22:29 -0800

    multithread->multiprocess (>10x speed up)

--- logextractor.py
+++ logextractor.py
@@ -1,9 +1,8 @@
+from contextlib import chdir
 import json
 import os
-import queue
 import sys
-import threading
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from ctypes import ArgumentError
 from dataclasses import asdict, dataclass
@@ -13,18 +12,35 @@ from typing import Optional
 from scanf import scanf_compile
 
 import clang.cindex as ci
-from clang.cindex import CompilationDatabase, CompilationDatabaseError, Index, TranslationUnit
+from clang.cindex import CompilationDatabase, CompilationDatabaseError
+
+LOG_FUNCS = {
+    'LogDebug', 'LogTrace',
+    'LogPrintf', 'LogPrint',
+    'LogInfo', 'LogError', 'LogWarning',
+    'LogPrintFormatInternal'
+}
 
 @dataclass(frozen=True, slots=True)
 class LogMessage:
     fmt: str                     # the format string (without surrounding quotes)
-    regex: tuple[str, list[str]]
+    regex: str
+    regex_types: list[str]
     file: Optional[str]         # source file name (None for built‑ins)
     line: int                    # line number of the macro call
     column: int                  # column number of the macro call
     macro: str                   # which Log* macro was used
     category: Optional[str] = None   # optional category argument
 
+
+def fmt_to_regex(fmt_str):
+    """
+    Converts a printf/scanf string to a Python Regex.
+    Returns the regex string and a list of inferred types, just a wrapper
+    for the python scanf package's scanf_compile.
+    """
+
+    return scanf_compile(fmt_str, collapseWhitespace=False)
 
 def arg_str(arg: list[ci.Token]) -> str:
     return "".join(tok.spelling for tok in arg)
@@ -33,7 +49,6 @@ def arg_str(arg: list[ci.Token]) -> str:
 # there is probably a better way to do this using the args to
 # LogPrintFormatInternal but afaict can't access the argumentizer for that from
 # this translation unit.
-
 def get_macro_args(cursor: ci.Cursor) -> list[list[ci.Token]]:
     macro_name = cursor.spelling
     toks = list(cursor.get_tokens())
@@ -68,6 +83,114 @@ def get_macro_args(cursor: ci.Cursor) -> list[list[ci.Token]]:
     return []
 
 
+# Global that will be initialized once in each process
+_PROCESS_LOCAL_INDEX = None
+
+
+def worker_init():
+    """
+    Called once per process to init the global per-process index.
+    """
+    global _PROCESS_LOCAL_INDEX
+    _PROCESS_LOCAL_INDEX = ci.Index.create()
+
+
+def worker_entrypoint(filename: str, args: list[str], root_dir: str):
+    """
+    Wrapper that initializes / retrieves the global _PROCESS_LOCAL_INDEX and
+    invokes parse_file.
+    """
+    global _PROCESS_LOCAL_INDEX
+    if _PROCESS_LOCAL_INDEX is None:
+        _PROCESS_LOCAL_INDEX = ci.Index.create()
+
+    return parse_file(filename, args, root_dir, _PROCESS_LOCAL_INDEX)
+
+
+def process_log(node: ci.Cursor, root_dir: str) -> LogMessage:
+    macro = node.spelling
+    args = get_macro_args(node)
+
+    category: Optional[str] = None
+
+    idx = 0
+    if macro in ("LogDebug", "LogTrace"):
+        # First arg is category
+        category = arg_str(args[idx])
+        idx += 1
+
+    fmt_str = arg_str(args[idx])
+    if fmt_str.startswith('"') and fmt_str.endswith('"'):
+        fmt_str = fmt_str[1:-1]
+    else:
+        # The format string is not a literal, probably not worth handling this
+        print(f"Format string is not a literal, skipped: {fmt_str}")
+
+    # on second thought, store the fmt strings in the text file,
+    # the log parser can compile to regex's at load time?
+    regex, regex_types = fmt_to_regex(fmt_str)
+    regex = regex.pattern
+    regex_types = [getattr(t, '__name__', str(t)) for t in regex_types]
+
+    loc = node.location
+    file_name = Path(loc.file.name).relative_to(root_dir)
+
+    return LogMessage(
+        fmt=fmt_str,
+        regex=regex,
+        regex_types=regex_types,
+        file=str(file_name),
+        line=loc.line,
+        column=loc.column,
+        macro=macro,
+        category=category,
+    )
+
+
+def parse_file(
+    filename: str, args: list[str], root_dir: str, index: Optional[ci.Index] = None
+    ) -> list[LogMessage]:
+    """
+    Parse file into LogMessages, optionally reuses an existing index if the
+    caller has one.
+    """
+    def visit_node(node: ci.Cursor, root_dir: str, results: list[LogMessage]):
+        if node.location.file:
+            fname = node.location.file.name
+            # skip files outside of our dir
+            if not fname.startswith(root_dir):
+                return
+
+        if node.kind == ci.CursorKind.MACRO_INSTANTIATION:
+            if node.spelling in LOG_FUNCS:
+                results.append(process_log(node, root_dir))
+
+        # Recurse
+        for child in node.get_children():
+            visit_node(child, root_dir, results)
+
+    results: list[LogMessage] = []
+
+    if not index:
+        index = ci.Index.create()
+
+    try:
+        tu = index.parse(
+            str(filename),
+            args=args,
+            options=ci.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD,
+        )
+    except ci.TranslationUnitLoadError as e:
+        print(f"Error parsing {filename}: {e}")
+        print(f"Using args: {" ".join(args)}")
+        return []
+
+    assert isinstance(tu.cursor, ci.Cursor)
+
+    visit_node(tu.cursor, root_dir, results)
+    return results
+
+
 class LogCompiler:
     def __init__(self, root_dir):
         self.root_dir = Path(root_dir)
@@ -88,132 +211,32 @@ class LogCompiler:
             raise ArgumentError(
                 f"Error: something went wrong loading {compile_commands_path}")
 
-        # reusing an index is just for performance reasons, cached headers
-        self.index = Index.create()
-
-        self.log_funcs = {
-            'LogDebug',
-            'LogTrace',
-            'LogPrintf',
-            'LogInfo',
-            'LogPrint',
-            'LogWarning',
-            'LogPrintFormatInternal'
-        }
-
-        self.lock = threading.Lock()
         self.log_messages: list[LogMessage] = []
 
-        self.num_threads = os.cpu_count() or 4
-
-        self.index_pool = queue.Queue()
-
-        for _ in range(self.num_threads):
-            self.index_pool.put(Index.create())
-
-
-
-    def fmt_to_regex(self, fmt_str):
-        """
-        Converts a printf/scanf string to a Python Regex.
-        Returns the regex string and a list of inferred types, just a wrapper
-        for the python scanf package's scanf_compile.
-        """
-
-        return scanf_compile(fmt_str, collapseWhitespace=False)
-
-    def visit_node(self, node: ci.Cursor):
-        if node.location.file:
-            fname = node.location.file.name
-            # skip files outside of our dir
-            if not fname.startswith(self.root_dir_str):
-                return
-
-        if node.kind.is_expression() or node.kind == ci.CursorKind.MACRO_INSTANTIATION:
-            if node.spelling in self.log_funcs:
-                self.process_log_call(node)
-
-        # Recurse
-        for child in node.get_children():
-            self.visit_node(child)
-
-    def process_log_call(self, node: ci.Cursor):
-        macro = node.spelling
-        args = get_macro_args(node)
-        # Todo: include metadata:
-        # category, source location
-
-        # What to do about duplicate fmt_str's?
-        # These have to be treated as one log message, maybe with multiple
-        # locations, or just pick one out of a hat?
-        category: Optional[str] = None
-
-        idx = 0
-        if macro == "LogDebug" or macro == "LogTrace":
-            # First arg is category
-            category = arg_str(args[idx])
-            idx += 1
-
-        fmt_str = arg_str(args[idx])
-        if fmt_str.startswith('"') and fmt_str.endswith('"'):
-            fmt_str = fmt_str[1:-1]
-        else:
-            # The format string is not a literal, probably not worth handling this
-            print(f"Format string is not a literal, skipped: {fmt_str}")
-
-        # on second thought, store the fmt strings in the text file,
-        # the log parser can compile to regex's at load time?
-        regex, regex_types = self.fmt_to_regex(fmt_str)
-        regex = regex.pattern
-        regex_types = [getattr(t, '__name__', str(t)) for t in regex_types]
-
-        loc = node.location
-        file_name = Path(loc.file.name) if loc.file else ""
-        file_name = str(file_name.relative_to(self.root_dir))
-
-        msg = LogMessage(
-            fmt=fmt_str,
-            regex=(regex, regex_types),
-            file=file_name,
-            line=loc.line,
-            column=loc.column,
-            macro=macro,
-            category=category,
-        )
-        with self.lock:
-            self.log_messages.append(msg)
-
-    def parse_worker(self, filename, args):
-        """Worker method for the thread pool."""
-        index = self.index_pool.get()
-
-        try:
-            tu = index.parse(
-                str(filename),
-                args=args,
-                options=TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD
-            )
-            self.visit_node(tu.cursor)
-            return filename
-        except Exception as e:
-            print(f"\nError parsing {filename}: {e}")
-            return None
-        finally:
-            self.index_pool.put(index)
-
     @staticmethod
-    def compile_args(basename, args):
+    def clean_args(args):
         arglist = list(args)[1:]
         clean = []
+        skip_next = False
+        # Flags that slow down parsing but aren't needed for AST generation
+        bad_prefixes = ('-O', '-W', '-g', '-f')
 
         for a in arglist:
             # stop parsing at -- {filename.cpp}
             if a == '--':
                 break
+            elif skip_next:
+                skip_next = False
+                continue
             # These break everything for some reason
-            elif not a.startswith('-') and a.endswith(basename):
+            elif a == '-c':
+                skip_next = True
+                continue
+            elif a.startswith(bad_prefixes):
                 continue
             clean.append(a)
+
+        clean.append('-fsyntax-only')
         return clean
 
     def parse_file(self, filename):
@@ -224,19 +247,10 @@ class LogCompiler:
             raise ArgumentError(f"{filename} not found in compilation database!")
 
         # We only want the first one if multiple exist.
-        args = LogCompiler.compile_args(os.path.basename(filename.absolute()), cmds[0].arguments)
+        args = LogCompiler.clean_args(cmds[0].arguments)
 
-        try:
-            tu = self.index.parse(
-                filename,
-                args=args,
-                options=TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
-        except ci.TranslationUnitLoadError as e:
-            print(e)
-            print(f"Something went wrong parsing {filename} with args: {args}")
-            raise
-
-        self.visit_node(tu.cursor)
+        index = ci.Index.create()
+        self.log_messages.extend(parse_file(str(filename), args, self.root_dir_str, index))
 
     def parse_all(self):
         exclude_dirs = [
@@ -261,30 +275,35 @@ class LogCompiler:
 
         for cmd in all_cmds:
             src_path = Path(cmd.filename).resolve()
+
             if src_path in seen:
                 continue
+
             seen.add(src_path)
             if any(src_path.is_relative_to(d) for d in exclude_dirs):
                 continue
             if src_path.suffix == ".cpp":
-                args = self.compile_args(os.path.basename(src_path.absolute()), cmd.arguments)
-                tasks.append((src_path, args))
+                args = self.clean_args(cmd.arguments)
+                tasks.append((src_path, args, self.root_dir_str))
 
         print(f"Parsing {len(tasks)} files using {os.cpu_count()} threads...")
 
-        with ThreadPoolExecutor() as executor:
+        workers = os.cpu_count()
+        with ProcessPoolExecutor(
+            max_workers=workers,
+            initializer=worker_init
+        ) as executor:
             # Submit all tasks
             futures = {
-                executor.submit(self.parse_worker, f, a): f
-                for f, a in tasks
+                executor.submit(worker_entrypoint, f, a, r): f
+                for f, a, r in tasks
             }
 
             # Watch for completion
             for i, future in enumerate(as_completed(futures)):
                 try:
-                    future.result() # Raises exceptions if the thread crashed
-                    if i % 10 == 0:
-                        print(f"Progress: {i}/{len(tasks)}", end='\r')
+                    self.log_messages.extend(future.result())
+                    print(f"Progress: {i}/{len(tasks)}", end='\r')
                 except Exception as e:
                     print(f"\nTask failed: {e}")
 
@@ -297,16 +316,17 @@ class LogCompiler:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        compiler = LogCompiler(sys.argv[1])
-        compiler.parse_all()
-    elif len(sys.argv) == 4:
-        compiler = LogCompiler(sys.argv[1])
-        compiler.parse_file(sys.argv[2])
-    else:
+    if len(sys.argv) not in [3, 4]:
         print(
             "Usage!!! logextractor.py {src_dir} {out_path}\n"
             "Or!!! logextractor.py {src_dir} {file} {out_path}\n"
             )
         sys.exit(-1)
-    compiler.dump_db(sys.argv[-1])
+
+    with chdir(sys.argv[1]):
+        compiler = LogCompiler(sys.argv[1])
+        if len(sys.argv) == 3:
+            compiler.parse_all()
+        elif len(sys.argv) == 4:
+            compiler.parse_file(sys.argv[2])
+        compiler.dump_db(sys.argv[-1])
